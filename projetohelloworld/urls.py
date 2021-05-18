d114cc17051e1b82eaf1139f0d9e7fc01d1444cc
carolinesales <caroline.vsales@gmail.com>
2021-05-18 20:38:38 +0000

    Atualização do login do site no github

--- projetohelloworld/urls.py
+++ projetohelloworld/urls.py
@@ -18,5 +18,17 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('helloworldapp.urls'))
+    #path('', include('helloworldapp.urls')),
+    path('', include('gestaoacademicaapp.urls')),
+]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    #path('', include('helloworldapp.urls')),
+    path('', include('gestaoacademicaapp.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+
 ]
